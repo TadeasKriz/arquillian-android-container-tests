0797158822348749e89455ce143b22358d503913
Stefan Miklosovic <smikloso@redhat.com>
2013-05-16 20:39:13 +0200

    Test with Selendroid

--- android-container-native-test-01/src/test/java/org/jboss/arquillian/android/container/test01/test/ContainerTest.java
+++ android-container-native-test-01/src/test/java/org/jboss/arquillian/android/container/test01/test/ContainerTest.java
@@ -1,14 +1,12 @@
 package org.jboss.arquillian.android.container.test01.test;
 
 
-import org.jboss.arquillian.android.container.test01.Bar;
-import org.jboss.arquillian.android.container.test01.Baz;
-import org.jboss.arquillian.android.container.test01.ObjectStore;
+import java.io.File;
+
 import org.jboss.arquillian.container.android.api.AndroidDevice;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.TargetsContainer;
-import org.jboss.arquillian.core.spi.HashObjectStore;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -19,39 +17,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.android.container.test01.Bar;
 
 @RunWith(Arquillian.class)
 @RunAsClient
 public class ContainerTest {
 
-    @Deployment(name = "android1", order = 1, testable = false)
-    @TargetsContainer("android1")
+    @Deployment(name = "android", testable = false)
+    @TargetsContainer("android")
     public static Archive<?> createDeployment1() {
-        System.out.println("create deployment android2");
-        return ShrinkWrap.create(JavaArchive.class, "android1.jar");
-    }
-
-    @Deployment(name = "jbossas", order = 2, testable = false)
-    @TargetsContainer("jbossas")
-    public static Archive<?> createDeployment2() {
-        System.out.println("create deployment jbossas");
-        return ShrinkWrap.create(JavaArchive.class, "jbossas.jar")
-            .addClass(Bar.class);
+        return ShrinkWrap.createFromZipFile(JavaArchive.class, new File("selendroid-test-app-0.4.apk"));
     }
 
     @Test
     @InSequence(1)
-    @OperateOnDeployment("android1")
+    @OperateOnDeployment("android")
     public void test01(@ArquillianResource AndroidDevice android) {
         Assert.assertTrue(android != null);
     }
-
-    @Test
-    @InSequence(2)
-    @OperateOnDeployment("jbossas")
-    public void test02() {
-        Assert.assertTrue(true);
-    }
-
 }
